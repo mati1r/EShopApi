04890114cc8aefd8d5edbc469a2f9fd4a5f190ea
mati1r <mati1r@wp.pl>
2026-02-14 15:50:38 +0100

    Photo extension migration

---
+++ Infrastracture/Data/Migrations/20260214145010_ProductPhotoExtension.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiTemplate.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductPhotoExtension : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Extenstion",
+                table: "ProductPhotos",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Extenstion",
+                table: "ProductPhotos");
+        }
+    }
+}
